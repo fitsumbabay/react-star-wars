a1c058cf98cad3e6fbe7ee481925805edf9e19ce
fitsumbabay <fitsumbabay@gmail.com>
2024-11-22 14:17:54 -0500

    update starshpCard.css and starshpCard.js

--- src/StarshipCard.js
+++ src/StarshipCard.js
@@ -1,10 +1,25 @@
 import React from "react";
-import "./StarshipCard.css"; 
+import "./StarshipCard.css"; // Optional: If you want to add specific styles for the card
 
 const StarshipCard = ({ starship }) => {
   return (
     <div className="starship-card">
       <h2>{starship.name}</h2>
+      <p>
+        <strong>Model:</strong> {starship.model}
+      </p>
+      <p>
+        <strong>Manufacturer:</strong> {starship.manufacturer}
+      </p>
+      <p>
+        <strong>Cost:</strong> {starship.cost_in_credits} credits
+      </p>
+      <p>
+        <strong>Crew:</strong> {starship.crew}
+      </p>
+      <p>
+        <strong>Passengers:</strong> {starship.passengers}
+      </p>
     </div>
   );
 };
